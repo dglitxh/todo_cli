08e567a44c25c39e8708cd94399d18ba452ba6ed
ydeezly <ydglitch@gmail.com>
2022-12-09 12:35:34 +0000

    upd: main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"cl_apps/todo"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 const todoFileName = ".todo.json"
 
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+		}
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+		}
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+	return s.Text(), nil
+}
+
 func main() {
 	// Parsing command line flags
-	task := flag.String("task", "", "Task to be included in the ToDo list")
 	desc := flag.String("desc", "", "Description of the task to be added")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 	flag.Parse()
@@ -42,13 +60,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	case *task != "":
-		// Add the task
-		l.AddTask(*task, *desc)
-		// Save the new list
-		if err := l.Save(todoFileName); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+	case *add:
+	// When any arguments (excluding flags) are provided, they will be
+	// used as the new task
+	t, err := getTask(os.Stdin, flag.Args()...)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	l.AddTask(t, *desc)
+	// Save the new list
+	if err := l.Save(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	default:
 		// Invalid flag provided
